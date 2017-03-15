5fb42a8588a3b05e799be4d9b6131d233046d68f
Dylan Elznic <djey96@mst.edu>
2017-03-15 15:55:17 -0500

    Add files via upload

---
+++ Magic 8 Ball Chrome Extension/8ballscript.js
@@ -0,0 +1,62 @@
+switch(Math.floor(Math.random() * 20) + 1) {
+	case 1:
+		document.write("It is certain");
+		break;
+	case 2:
+		document.write("It is decidedly so");
+		break;
+	case 3:
+		document.write("Without a doubt");
+		break;
+	case 4:
+		document.write("Yes, definitely");
+		break;
+	case 5:
+		document.write("You may rely on it");
+		break;
+	case 6:
+		document.write("As I see it, yes");
+		break;
+	case 7:
+		document.write("Most likely");
+		break;
+	case 8:
+		document.write("Outlook good");
+		break;
+	case 9:
+		document.write("Yes");
+		break;
+	case 10:
+		document.write("Signs point to yes");
+		break;
+	case 11:
+		document.write("Reply hazy try again");
+		break;
+	case 12:
+		document.write("Ask again later");
+		break;
+	case 13:
+		document.write("Better not tell you now");
+		break;
+	case 14:
+		document.write("Cannot predict how");
+		break;
+	case 15:
+		document.write("Concentrate and ask again");
+		break;
+	case 16:
+		document.write("Don't count on it");
+		break;
+	case 17:
+		document.write("My reply is no");
+		break;
+	case 18:
+		document.write("My sources say no");
+		break;
+	case 19:
+		document.write("Outlook not so good");
+		break;
+	case 20:
+		document.write("Very doubtful");
+		break;
+}
